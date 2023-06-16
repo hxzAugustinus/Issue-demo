d07ac0cc54faa16c8732f234eab96614fc988e21
hxzAugustinus <13638682603@163.com>
2023-06-16 10:13:16 +0800

    build(buildConfig): change  publicPath   case

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@
  * @Author: hxz
  * @Date: 2021-10-29 15:46:02
  * @LastEditors: hxz
- * @LastEditTime: 2023-06-16 10:07:36
+ * @LastEditTime: 2023-06-16 10:12:02
  * @Description:
  */
 module.exports = {
 	lintOnSave: true,
 	productionSourceMap: false, // 隐藏源码
-	publicPath: '/issue-demo/', // 默认'/'，部署应用包时的基本 URL
+	publicPath: '/Issue-demo/', // 默认'/'，部署应用包时的基本 URL
 	outputDir: 'docs',
 };
